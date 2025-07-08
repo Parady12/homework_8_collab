b12a873d8a5adcf6f281e1460b3106bf9e3780fa
Parady12 <2024568hok@aupp.edu.kh>
2025-07-08 12:05:36 +0700

    exercise1 done

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,11 @@
 def replace_last(numbers):
-    ...
+    result = []
+    if len(numbers) > 0:
+        result.append(numbers[-1])
+        numbers.remove(numbers[-1])
+        for i in numbers:
+            result.append(i)
+    else:
+        return numbers
+    return result
+print(replace_last([]))
