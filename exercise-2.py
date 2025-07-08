1bc84880fb167e05177b133d93ab63b94624eec3
Parady12 <2024568hok@aupp.edu.kh>
2025-07-08 20:18:28 +0700

    exercise 2 second done

--- exercise-2.py
+++ exercise-2.py
@@ -4,6 +4,7 @@ def index_power(numnbers, n):
         return result
     except:
         return -1
+    
 print(index_power([1, 2, 3, 4], 2))
 print(index_power([1, 3, 10, 100], 3))
 print(index_power([0, 1], 0))
