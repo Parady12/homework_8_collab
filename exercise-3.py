fe12cbb25ba7ccb7dd5d3abcc708ba219bfba612
Leng2025 <2024579hun@aupp.edu.kh>
2025-07-08 19:51:17 +0700

    exercise3byleng

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,13 @@
 def remove_all_after(numbers, n):
     ...
+    result = []
+    for item in numbers: 
+        result.append(item)
+        if item == n:
+            break
+    return result
+
+# print(remove_all_after([1, 2, 3, 4, 5], 3))
+# print(remove_all_after([1, 1, 2, 2, 3, 3], 2))
+# print(remove_all_after([], 3))
+# print(remove_all_after([1, 2, 3], 8))
