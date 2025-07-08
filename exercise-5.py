1231c815d65c58c7af00a4d2feb928fee9e28945
Parady12 <2024568hok@aupp.edu.kh>
2025-07-08 23:56:57 +0700

    exercise 5 done

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,8 @@
 def reverse_ascending(numbers):
-    ...
+    sorted_result = sorted(numbers)
+    reversed_result = reversed(sorted_result)
+    return list(reversed_result)
+print(reverse_ascending([1, 2, 3, 4, 5]))
+print(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3]))
+print(reverse_ascending([5, 4, 3, 2, 1]))
+print(reverse_ascending([]))
